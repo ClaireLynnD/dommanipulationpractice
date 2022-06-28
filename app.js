ad18ff694d3c367098b46837419c2c1799c1abd3
roesnera <adam.roesner@generaiton.org>
2022-06-28 16:23:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Connected!');
+
+// Write your DOM manipulation here
+
+// write your even handler function and inside it . . .
+
+// prevent the default action from firing
+
+// select your HTML Elements using DOM methods
+
+// update the text of your elements with a randomly selected quote
+
+
+
+// Add an event listener to the button
